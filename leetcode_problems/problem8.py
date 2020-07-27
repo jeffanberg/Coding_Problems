974001641cf4544ab2fb5129ce41da25178eff86
Jeff Anberg <jeffanberg@gmail.com>
2020-07-26 22:58:47 -0700

    Leetcode Problem 8 Solution

--- leetcode_problems/problem8.py
+++ leetcode_problems/problem8.py
@@ -19,13 +19,42 @@ Only the space character ' ' is considered as whitespace character.
 Assume we are dealing with an environment which could only store integers within
 the 32-bit signed integer range: [−231,  231 − 1].
 If the numerical value is out of the range of representable values, INT_MAX (231 − 1) or INT_MIN (−231) is returned.'''
+import re
 
 class Solution:
     def myAtoi(self, str: str) -> int:
         res = str
+        num = ''
+        negative = False
+        if not re.findall('[0-9]', res):
+                return 0
         while True:
             if res[0] == ' ':
-               res = res[:1]
+                res = res[1:]
+            elif res[0] == '-' and re.findall('[0-9]', res[1]):
+                negative = True
+                res = res[1:]
+            elif res[0] == '+' and re.findall('[0-9]', res[1]):
+                res = res[1:]
+            elif re.findall('[0-9]', res[0]):
+                num = num + res[0]
+                if len(res) > 1 and re.findall('[0-9]', res[1]):
+                    res = res[1:]
+                else:
+                    break
             else:
-                break 
-        return 0
+                break
+        if len(num) > 0:
+            num = int(num)
+        else:
+            return 0
+        if negative:
+            num = num * -1
+        if num > pow(2,31) - 1:
+            return pow(2,31) - 1
+        elif num < pow(2,31) * -1:
+            return pow(2,31) * -1
+        else:
+            return num
+    
+print(Solution().myAtoi("0-1"))

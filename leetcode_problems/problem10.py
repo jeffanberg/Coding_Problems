73550e8ca17c68776dab591e9bcacc5ae963f451
jeffanberg <jeff@penguin>
2020-08-08 19:55:13 -0700

    leetcode problem 10 progress

--- leetcode_problems/problem10.py
+++ leetcode_problems/problem10.py
@@ -10,5 +10,32 @@ Note:
 s could be empty and contains only lowercase letters a-z.
 p could be empty and contains only lowercase letters a-z, and characters like . or *.'''
 
-def isMatch(self, s: str, p: str) -> bool:
-    return True
+
+def isMatch(s: str, p: str) -> bool:
+    stillMatches = True
+    s_index = 0
+    for char in range(len(p)):
+        if char < len(p) - 1:
+            if p[char + 1] == "*":
+                continue
+
+        if p[char] == ".":
+            s_index += 1
+            continue
+        elif p[char] == "*":
+            if s[s_index] == p[char - 1]:
+                s_index += 1
+                continue
+        else:
+            if s[s_index] != p[char]:
+                stillMatches = False
+            else:
+                s_index += 1
+    if s_index != len(s) - 1:
+        stillMatches = False      
+    return stillMatches
+
+s = "aaaaaa"
+p = "a*"
+
+print(isMatch(s,p))

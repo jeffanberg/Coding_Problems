adabe4c19a49874c5069998c31a597e4a03d3a85
Jeff Anberg <jeffanberg@gmail.com>
2020-09-10 01:49:22 -0700

    Euler Problem 19 solution

--- project_euler/problem19.py
+++ project_euler/problem19.py
@@ -17,4 +17,55 @@ How many Sundays fell on the first of the month during the twentieth century
 (1 Jan 1901 to 31 Dec 2000)?
 '''
 
+firstdays = dict()
 
+# 1 = sunday, 2 = monday, 3 = tuesday, etc.
+# Jan. 1901 was a Tuesday
+day = 3
+
+
+def advanceWeek(currentday, num):
+    start = currentday
+    count = 0
+    while count < num:
+        if start == 7:
+            start = 1
+            count += 1
+        else:
+            start += 1
+            count += 1
+    return start
+
+# print(advanceWeek(5, 28))
+
+
+def count(values, item):
+    count = 0
+    for each in values:
+        if each == item:
+            count += 1
+    return count
+
+
+for year in range(1901, 2001, 1):
+    for month in range(1, 13):
+        if month in [4, 6, 9, 11]:
+            key_string = str(month) + '-' + str(year)
+            firstdays.update({key_string: day})
+            day = advanceWeek(day, 30)
+        elif month == 2:
+            key_string = str(month) + '-' + str(year)
+            firstdays.update({key_string: day})
+            if year % 400 == 0:
+                day = advanceWeek(day, 29)
+            elif year % 4 == 0 and not year % 100 == 0:
+                day = advanceWeek(day, 29)
+            else:
+                day = advanceWeek(day, 28)
+        else:
+            key_string = str(month) + '-' + str(year)
+            firstdays.update({key_string: day})
+            day = advanceWeek(day, 31)
+
+print(firstdays)
+print(count(firstdays.values(), 1))

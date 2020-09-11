61264be0cd8fbc4b72002cd96e4eecc116416d60
jeffanberg <jeff@penguin>
2020-09-10 17:57:19 -0700

    Euler P21 optimized solution

--- project_euler/problem21.py
+++ project_euler/problem21.py
@@ -10,34 +10,26 @@ The proper divisors of 284 are 1, 2, 4, 71 and 142; so d(284) = 220.
 
 Evaluate the sum of all the amicable numbers under 10000.
 '''
-
+import math
 
 def properDivisor(x):
-    tempList = []
-    for num in range(1, x):
-        if (x / num) % 1 == 0:
-            tempList.append(num)
-    return sum(tempList)
+    s = 1
+    for num in range(2, int(math.sqrt(x)) + 1):
+        if x % num == 0:
+            s += num
+            s += x / num
+    return s
 
 
 def findAmicableNums(limit):
     amicableNums = set()
-    checked = set()
-    for x in range(limit, 1, -1):
-        if x not in checked:
-            print('Checking: ' + str(x))
-            possibleami = properDivisor(x)
-            possibleami2 = properDivisor(possibleami)
-            checked.add(possibleami2)
-            checked.add(possibleami)
-            if possibleami2 == possibleami:
-                amicableNums.add(possibleami)
-                amicableNums.add(possibleami2)
+    for x in range(limit, 0, -1):
+        possibleami = properDivisor(x)
+        if x != possibleami and properDivisor(possibleami) == x:
+            amicableNums.add(x)
+            amicableNums.add(possibleami)
     return amicableNums
 
-
-print(properDivisor(220))
-print(properDivisor(properDivisor(220)))
-
-# print(findAmicableNums(1000))
-# print(sum(findAmicableNums(10000)))
+ami = findAmicableNums(10000)
+print(ami)
+print(sum(ami))

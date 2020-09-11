046cd9e383dabc89211508dee9190d72092bdaaf
jeffanberg <jeff@penguin>
2020-09-10 21:17:34 -0700

    Euler P22 Solution

---
+++ project_euler/problem22.py
@@ -0,0 +1,43 @@
+'''
+Using names.txt (right click and 'Save Link/Target As...'),
+a 46K text file containing over five-thousand first names,
+begin by sorting it into alphabetical order.
+Then working out the alphabetical value for each name,
+multiply this value by its alphabetical position in the list to obtain a name score.
+
+For example, when the list is sorted into alphabetical order,
+COLIN, which is worth 3 + 15 + 12 + 9 + 14 = 53, is the 938th name in the list.
+So, COLIN would obtain a score of 938 × 53 = 49714.
+
+What is the total of all the name scores in the file?
+'''
+
+import os
+
+__location__ = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+with open(os.path.join(__location__, 'p022_names.txt')) as names_file:
+    names = names_file.read().replace('"','').split(',')
+
+def convertCharToInt(char):
+    stringtoint = {'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'G': 7, 
+    'H': 8, 'I': 9, 'J': 10, 'K': 11, 'L': 12, 'M': 13, 'N': 14, 'O': 15,
+    'P': 16, 'Q': 17, 'R': 18, 'S': 19, 'T': 20, 'U': 21, 'V': 22,
+    'W': 23, 'X': 24, 'Y': 25, 'Z': 26 
+    }
+    return stringtoint.get(char)
+            
+sorted_names = sorted(names)      
+
+def totalSum(alpha_names):
+    totalsum = 0
+    for i in alpha_names:
+        wordsum = 0
+        for x in i:
+            wordsum += convertCharToInt(x)
+        wordsum = wordsum * (alpha_names.index(i) + 1)
+        totalsum += wordsum
+    return totalsum
+
+print(totalSum(sorted_names))

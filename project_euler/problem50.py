274cd0f4a4f263a55ef6bf5715238284eb862927
Jeff Anberg <jeffanberg@gmail.com>
2020-10-11 00:33:39 -0700

    Euler 50 Solution

--- project_euler/problem50.py
+++ project_euler/problem50.py
@@ -60,19 +60,44 @@ def findConsecutivePrimeSum(limit):
     prime_list = generatePrimes(limit)
     if limit % 2 == 0:
         limit -= 1
-    for n in range(limit, 13, -1):
+    for n in range(limit, 1000, -1):
         if isPrime(n):
             for c in range(0, len(prime_list)):
                 count = 0
                 while count < len(prime_list):
                     seq = prime_list[c:len(prime_list) - count]
-                    if sum(seq) == n and len(seq) > sum_length:
-                        ans = n
-                        sum_length = len(seq)
-                        print(f'length: {sum_length} prime: {ans}')
-                        break
+                    if len(seq) > sum_length:
+                        if sum(seq) == n:
+                            ans = n
+                            sum_length = len(seq)
+                            print(f'length: {sum_length} prime: {ans}')
+                            break
                     count += 1
     return ans
 
 
-print(findConsecutivePrimeSum(10000))
+def findLongestPrimeSum(limit):
+    prime_list = [2, 3, 5, 7, 11]
+    n = 13
+    while sum(prime_list) < limit:
+        if isPrime(n):
+            prime_list.append(n)
+        n = n + 2
+    seq = []
+    p_len = len(prime_list)
+    i = p_len
+    while i != 0:
+        n = 0
+        while n + i < p_len + 1:
+            temp_seq = prime_list[n: n+i]
+            if sum(temp_seq) <= limit:
+                if isPrime(sum(temp_seq)):
+                    if len(temp_seq) > len(seq):
+                        seq = temp_seq
+            n = n + 1
+        i = i - 1
+    return sum(seq)
+
+
+# print(findConsecutivePrimeSum(1000))
+print(findLongestPrimeSum(1000000))

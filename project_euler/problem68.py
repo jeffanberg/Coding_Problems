6569b6f0c43e45c84665cffe8f6d8b4ad553d532
Jeff Anberg <jeffanberg@gmail.com>
2020-11-03 12:25:33 -0800

    Euler 68 Set up

---
+++ project_euler/problem68.py
@@ -0,0 +1,15 @@
+'''
+Magic 5-gon ring
+https://projecteuler.net/problem=68
+
+By concatenating each group it is possible to form 9-digit strings;
+the maximum string for a 3-gon ring is 432621513.
+
+Using the numbers 1 to 10, and depending on arrangements,
+it is possible to form 16- and 17-digit strings.
+What is the maximum 16-digit string for a "magic" 5-gon ring?
+'''
+
+
+def create3gon():
+    pass

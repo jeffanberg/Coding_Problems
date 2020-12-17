aaab79bf7960bed0a8854b85020c4fc7a5cc7f06
Jeff Anberg <jeffanberg@gmail.com>
2020-12-17 00:13:03 -0800

    Euler 69 Solution

--- project_euler/problem69.py
+++ project_euler/problem69.py
@@ -8,30 +8,32 @@ It can be seen that n=6 produces a maximum n/φ(n) for n ≤ 10.
 
 Find the value of n ≤ 1,000,000 for which n/φ(n) is a maximum.
 '''
-# from math import gcd
-
-
-def gcd(a, b):
-    if b == 0:
-        return a
-    else:
-        return gcd(b, a % b)
-
-
-def findTotients(num):
-    totients = 0
-    for i in range(1, num):
-        if gcd(i, num) == 1:
-            totients += 1
-    return totients
+# Using Euler's product formula, could be better optimized filtering for primes
 
 
 def findTotientMaximum(limit):
     maximum = 0
+    ans = 0
     for i in range(2, limit + 1):
-        if (i / findTotients(i)) > maximum:
-            maximum = i
-    return maximum
+        e = i / findTotientProduct(i)
+        if e > maximum:
+            maximum = e
+            ans = i
+    return ans
+
+
+def findTotientProduct(num):
+    result = num
+    p = 2
+    while p * p <= num:
+        if num % p == 0:
+            while num % p == 0:
+                num = num // p
+            result = result * (1.0 - (1.0 / float(p)))
+        p = p + 1
+    if num > 1:
+        result = result * (1.0 - (1.0 / float(num)))
+    return int(result)
 
 
 print(findTotientMaximum(1000000))

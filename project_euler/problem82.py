d91f4b14305057590774e86d073d507af930ba7f
Jeff Anberg <jeffanberg@gmail.com>
2021-10-03 18:20:33 -0700

    Euler problem 83. Used same pathfinding algorithm but with more movement.

---
+++ project_euler/problem82.py
@@ -0,0 +1,129 @@
+''' Project Euler Problem 83
+Find the minimal path sum from the top left to the bottom right by moving left,
+right, up, and down in matrix.txt (right click and "Save Link/Target As..."),
+a 31K text file containing an 80 by 80 matrix.
+'''
+
+import os
+import heapq
+
+__location__ = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+with open(os.path.join(__location__, 'p083_matrix.txt')) as matrix_file:
+    matrix = [list(map(int, n.split(','))) for n in
+              matrix_file.read().splitlines()]
+
+
+''' Using A Star Pathfinding, with guide from-
+https://www.redblobgames.com/pathfinding/a-star/implementation.html '''
+
+
+class PriorityQueue:
+    def __init__(self):
+        self.elements = []
+
+    def empty(self) -> bool:
+        return not self.elements
+
+    def put(self, item, priority: float):
+        heapq.heappush(self.elements, (priority, item))
+
+    def get(self):
+        return heapq.heappop(self.elements)[1]
+
+
+class Location:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __repr__(self) -> str:
+        return f'Location: x={self.x} y={self.y}'
+
+    def __hash__(self) -> int:
+        return hash((self.x, self.y))
+
+    def __eq__(self, other) -> bool:
+        return self.x == other.x and self.y == other.y
+
+    def __lt__(self, other):
+        return 0
+
+
+class Matrix:
+    def __init__(self):
+        self.data = matrix
+
+    @property
+    def width(self):
+        return len(self.data[0]) if self.data else 0
+
+    @property
+    def height(self):
+        return len(self.data)
+
+    def get(self, location):
+        return self.data[location.y][location.x]
+
+    def neighbors(self, location):
+        neighbors = []
+
+        if location.x < self.width - 1:
+            neighbors.append(Location(location.x + 1, location.y))
+
+        if location.y < self.height - 1:
+            neighbors.append(Location(location.x, location.y + 1))
+
+        if location.x - 1 > -1:
+            neighbors.append(Location(location.x - 1, location.y))
+
+        if location.y - 1 > -1:
+            neighbors.append(Location(location.x, location.y - 1))
+
+        return neighbors
+
+    def find_path(self, start, end):
+        frontier = PriorityQueue()
+        frontier.put(start, self.get(start))
+        came_from = dict()
+        cost_so_far = dict()
+        came_from[start] = None
+        cost_so_far[start] = self.get(start)
+
+        while not frontier.empty():
+            current = frontier.get()
+
+            if current == end:
+                break
+
+            for next in self.neighbors(current):
+                new_cost = cost_so_far[current] + self.get(next)
+                if next not in cost_so_far or new_cost < cost_so_far[next]:
+                    cost_so_far[next] = new_cost
+                    priority = new_cost
+                    frontier.put(next, priority)
+                    came_from[next] = current
+
+        path = [end]
+        while path[-1] != start:
+            path.append(came_from[path[-1]])
+            path.reverse()
+
+        return path, cost_so_far[end]
+
+    def print_path(self, path):
+        for y in range(self.height):
+            line = ''
+            for x in range(self.width):
+                location = Location(x, y)
+                if location in path:
+                    line += 'x'
+                else:
+                    line += '.'
+            print(line)
+
+
+path, answer = Matrix.find_path(Matrix(), Location(0, 0), Location(79, 79))
+Matrix.print_path(Matrix(), path)
+print(answer)
